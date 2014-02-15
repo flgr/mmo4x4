347d358664deb2432e6efec3de13e41aad3c1a76
Wolfgang Schell <github@jetztgrad.net>
2014-02-15 15:49:31 +0100

    implemented creation of new games
    - added class Game to backend.js
    - menu "Game -> start new" now creates a new game and lists all games

--- backend.js
+++ backend.js
@@ -10,10 +10,43 @@ var io = require('socket.io').listen(server);
 
 var port = 1537;
 server.listen(port);
-util.log(util.format("Server ready at http://localhost:%d/", port))
+util.log(util.format("Server ready at http://localhost:%d/", port));
+
+
+function Game(x,y) {
+    this.name = "New Game";
+    this.players = [];
+    this.board = [];
+    for (var i = 0; i < x; i++) {
+        this.board[i] = []
+        for (var j = 0; j < y; j++) {
+            this.board[i][j] = '';
+        }
+    }   
+}
+
+Game.prototype.toString = function() {
+    return this.name;
+}
+
+Game.prototype.put = function(x,y,player) {
+    if (this.board[x][y].length) {
+        throw "already taken";
+    }
+    else {
+        this.board[x][y] = player;
+    }
+}
+Game.prototype.finished = function() {
+    // TODO determine whether one player has won
+    return false;
+}
+
 
 function Server() {
     this.clients = {};
+    this.name = 'New Game';
+    this.games = [];
 }
 
 Server.prototype.pushClientUpdate = function(socket, changedClientNick) {
@@ -35,6 +68,27 @@ Server.prototype.onClientDisconnect = function(socket) {
     this.pushClientUpdate(socket, socket.client.nick);
 };
 
+Server.prototype.pushGameUpdate = function(socket, changedGame) {
+    socket.broadcast.emit("game update", this.games, changedGame);
+};
+
+
+Server.prototype.onNewGame = function(socket, data) {
+    var game = new Game(data.x, data.y);
+    if ("name" in data) 
+        game.name = data.name;
+    game.players.push(socket.client);
+    this.games.push(game);
+    console.info("games " + this.games.join(", "));
+    console.info("Added game " + socket.client.nick + " (" + socket.id + ")" +
+        ". Now have " + Object.keys(this.games).length);
+    this.pushGameUpdate(socket, game);
+    socket.emit("game update", this.games, game);
+}
+
+Server.prototype.onDeleteGame = function(socket) {
+}
+
 var server = new Server();
 
 io.sockets.on('connection', function(socket) {
@@ -46,6 +100,10 @@ io.sockets.on('connection', function(socket) {
     	if ("client" in socket) {
             server.onClientDisconnect(socket);
 	    }
+    });
+
+    socket.on('new-game', function(data) {
+        server.onNewGame(socket, data);
     });
 
     socket.emit("connect");
--- client.js
+++ client.js
@@ -4,6 +4,10 @@ $(window).load(function() {
 	console.info("Window load called");
 	var socket = io.connect('/');
 
+	$('[data-js="start-new-game"]').on('click', function(e) {
+		socket.emit('new-game', { x: 5, y: 3 });
+	});
+
 	socket.on('connect', function (data) {
 		console.info("Socket connected");
 
@@ -16,6 +20,17 @@ $(window).load(function() {
 				var sortedNick = sortedNicks[i];
 				$('<li/>', { class: 'player', text: sortedNick }).appendTo('#players');
 			}
+		});
+
+		socket.on('game update', function(games, changedGame) {
+			console.info('game update', changedGame);
+			var gamesDiv = $('#games');
+
+			gamesDiv.empty();
+
+			games.forEach(function(game) {
+				gamesDiv.append("<div><span class='glyphicon glyphicon-send'> </span> " + game.name + "</div>");
+			})
 		});
 
 		socket.emit('join', { nick: nick });
