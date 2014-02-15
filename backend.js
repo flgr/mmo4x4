3a5719e07fdc04c41f26fc3bc0aaa219349cc3e0
Florian Gross <Florian.S.Gross@web.de>
2014-02-15 11:47:18 +0100

    Update

--- backend.js
+++ backend.js
@@ -11,3 +11,22 @@ var io = require('socket.io').listen(server);
 var port = 1537;
 server.listen(port);
 util.log(util.format("Server ready at http://localhost:%d/", port))
+
+var clients = {};
+
+io.sockets.on('connection', function(socket) {
+    socket.on('join', function(data) {
+    	socket.client = { nick: data.nick };
+    	socket.emit('welcome', {});
+
+    	clients[data.nick] = socket.client;
+        console.info("Adding client " + socket.client.nick + " (" + socket.id + ")" + ". Now have " + Object.keys(clients).length);
+        console.info(clients);
+    });
+
+    socket.on('disconnect', function() {
+    	if ("client" in socket) {
+	    	delete clients[socket.client.nick];
+	    }
+    });
+});
